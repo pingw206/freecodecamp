2f8d89ff9cd9d986d6e6f6e729b25de842dcad5a
wenping <pingw206@gmail.com>
2020-03-21 22:46:55 +0800

    origin version

--- front-end-libraries-projects/markdown-previewer/src/toolbar.js
+++ front-end-libraries-projects/markdown-previewer/src/toolbar.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Toolbar = (props) => {
   return (
-    <div className="toolbar">
-      <i title="no-stack-dub-sack"/>      
+    <div className="toolbar">  
       {props.text}
       <i onClick={props.onClick} className={props.icon}></i>
     </div>
  )
 }
-  export default Toolbar;
+export default Toolbar;
